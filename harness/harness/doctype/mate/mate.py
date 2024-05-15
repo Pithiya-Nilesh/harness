d9ac2dd54ad5202e600d036fcea37cdb642758c4
Srushti238 <srushti@sanskartechnolab.com>
2024-05-15 19:39:53 +0530

    removed child tables and json edit

--- harness/harness/doctype/mate/mate.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2024, Harness and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-class Mate(Document):
-	pass
