0da926db1ea94c6356a0b5fff04b0bd3afbc7ff2
Pithiya-Nilesh <pithiyanilesh87@gmail.com>
2024-06-18 19:09:31 +0530

    price list

--- harness/public/js/item_price.js
+++ harness/public/js/item_price.js
@@ -18,11 +18,10 @@ function autoCreatePricingRowIfNotServiceItem(frm){
             if (response.message) {
                 var maintain_stock = response.message.is_stock_item;
                 if (maintain_stock){
-                    removeExtraRows(frm, 0)
-                    addRows(frm)
+                    addRowsForMaterialItem(frm)
                 }
                 else{
-                    removeExtraRows(frm, 1)
+                    addRowsForServiceItem(frm)
                 }
             }
         }
@@ -30,73 +29,82 @@ function autoCreatePricingRowIfNotServiceItem(frm){
 }
 
 
-function addRows(frm){
+function addRowsForMaterialItem(frm){
     var numRows = frm.doc.custom_qty_wise_rate ? frm.doc.custom_qty_wise_rate.length : 0;
-        console.log("Number of rows in custom_qty_wise_rate:", numRows);
-        if  (frm.doc.__islocal && numRows == 0){
-            frm.add_child("custom_qty_wise_rate",{
-                quantity: 1
-            });
-            frm.add_child("custom_qty_wise_rate",{
-                quantity: 2
-            });
-            frm.add_child("custom_qty_wise_rate",{
-                quantity: 3
-            });
-            frm.add_child("custom_qty_wise_rate",{
-                quantity: 4
-            });
-            frm.add_child("custom_qty_wise_rate",{
-                quantity: 5
-            });
-            frm.add_child("custom_qty_wise_rate",{
-                quantity: 6
-            });
-            frm.add_child("custom_qty_wise_rate",{
-                quantity: 7
-            });
-            frm.add_child("custom_qty_wise_rate",{
-                quantity: 10
-            });
-            frm.add_child("custom_qty_wise_rate",{
-                quantity: 15
-            });
-            frm.add_child("custom_qty_wise_rate",{
-                quantity: 20
-            });
-            frm.add_child("custom_qty_wise_rate",{
-                quantity: 30
-            });
-            frm.add_child("custom_qty_wise_rate",{
-                quantity: 50
-            });
-            frm.add_child("custom_qty_wise_rate",{
-                quantity: 100
-            });
-            frm.refresh_field("custom_qty_wise_rate");
-        }
-}
-
-function removeExtraRows(frm, id) {
-    var rows = frm.doc.custom_qty_wise_rate || [];
-
-    // Check if there are more than one row
-    if (rows.length > 1) {
-        // Remove extra rows starting from the second row
-        for (var i = rows.length - id; i > 0; i--) {
-            var row = rows[i];
-            if (row && row.$row) {
-                row.$row.remove();  // Remove the row from the DOM
-            }
-            frappe.model.clear_doc(row.doctype, row.name);  // Clear the row from the model
-        }
-        
-        // Keep only the first row in the array
-        frm.doc.custom_qty_wise_rate = [rows[0]];
-        
-        // Refresh the table field to reflect the change in the form
+    console.log("Number of rows in custom_qty_wise_rate:", numRows);
+    if (frm.doc.__islocal && numRows == 0){
+        frm.add_child("custom_qty_wise_rate",{
+            quantity: 1
+        });
+        frm.add_child("custom_qty_wise_rate",{
+            quantity: 2
+        });
+        frm.add_child("custom_qty_wise_rate",{
+            quantity: 3
+        });
+        frm.add_child("custom_qty_wise_rate",{
+            quantity: 4
+        });
+        frm.add_child("custom_qty_wise_rate",{
+            quantity: 5
+        });
+        frm.add_child("custom_qty_wise_rate",{
+            quantity: 6
+        });
+        frm.add_child("custom_qty_wise_rate",{
+            quantity: 7
+        });
+        frm.add_child("custom_qty_wise_rate",{
+            quantity: 10
+        });
+        frm.add_child("custom_qty_wise_rate",{
+            quantity: 15
+        });
+        frm.add_child("custom_qty_wise_rate",{
+            quantity: 20
+        });
+        frm.add_child("custom_qty_wise_rate",{
+            quantity: 30
+        });
+        frm.add_child("custom_qty_wise_rate",{
+            quantity: 50
+        });
+        frm.add_child("custom_qty_wise_rate",{
+            quantity: 100
+        });
         frm.refresh_field("custom_qty_wise_rate");
     }
+    else{
+        removeRows(frm)
+        addRowsForMaterialItem(frm)
+    }
+}
+
+function addRowsForServiceItem(frm) {
+    var numRows = frm.doc.custom_qty_wise_rate ? frm.doc.custom_qty_wise_rate.length : 0;
+    console.log("Number of rows in custom_qty_wise_rate:", numRows);
+    if (frm.doc.__islocal && numRows == 0){
+        frm.add_child("custom_qty_wise_rate",{
+            quantity: 1
+        });
+        frm.refresh_field('custom_qty_wise_rate')
+    }
+    else{
+        removeRows(frm)
+        addRowsForServiceItem(frm)
+    }
+}
+
+function removeRows(frm){
+    var rows = frm.doc.custom_qty_wise_rate || [];
+    for (var i = rows.length; i > 0; i--) {
+        var row = rows[i];
+        if (row && row.$row) {
+            row.$row.remove();
+        }
+        frappe.model.clear_doc(row.doctype, row.name); 
+    }
+    frm.doc.custom_qty_wise_rate = [rows[0]];
     frm.refresh_field("custom_qty_wise_rate");
 }
 
