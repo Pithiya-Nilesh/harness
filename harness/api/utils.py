9597fb2784f457889ca6003bebfd641c51a3698b
Pithiya-Nilesh <pithiyanilesh87@gmail.com>
2024-06-27 18:04:12 +0530

    unassigned item

--- harness/api/utils.py
+++ harness/api/utils.py
@@ -65,7 +65,7 @@ def set_section_name_in_db(doc, method):
     # don`t allow user to save data with unassigned item in items child table.
     try:
         for item in doc.items:
-            if item.item_code == "Unassigned":
+            if item.item_code == "Unassigned Item":
                 frappe.throw("Please replace Unassigned item with real item.")
     except Exception as e:
         frappe.log_error("Error: While not allow to save with unassigned item", e, doc.doctype, doc.name)
