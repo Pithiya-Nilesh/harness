bd2e9a494d4325f10b8a5a52c00d44d914aa0dba
Pithiya-Nilesh <pithiyanilesh87@gmail.com>
2024-06-29 19:33:23 +0530

    map fields

--- harness/public/js/timesheet.js
+++ harness/public/js/timesheet.js
@@ -1,5 +1,6 @@
 frappe.ui.form.on("Timesheet", {
     refresh: function(frm){
+        get_fields(frm);
         frm.fields_dict['time_logs'].grid.get_field('custom_service_item').get_query = function(doc, cdt, cdn) {
             return {
                 filters: [
@@ -8,4 +9,47 @@ frappe.ui.form.on("Timesheet", {
             };
         };
     },
+
+    before_save(frm) {
+        get_fields(frm);
+    },
+
+    after_save(frm) {
+        get_fields(frm);
+    },
+
+    before_submit(frm) {
+        get_fields(frm);
+    },
+
+    on_submit(frm) {
+        get_fields(frm);
+    },
+
+    onload:function(frm){
+         get_fields(frm);
+    }
 })
+
+
+
+
+function get_fields(frm) {
+
+    var fields = {
+        'Timesheet Detail':
+            [
+                { fieldname: 'activity_type', columns: 2 },
+                { fieldname: 'custom_service_item', columns: 2 },
+                { fieldname: 'task', columns: 1 },
+                { fieldname: 'hours', columns: 1 },
+                { fieldname: 'from_time', columns: 2 },
+                { fieldname: 'to_time', columns: 2 },
+            ]
+    }
+      
+    frappe.model.user_settings.save(frm.doctype, "GridView",fields).then((r) => {
+        frappe.model.user_settings[frm.doctype] = r.message || r;
+        frm.fields_dict.items.grid.reset_grid();
+    });
+}
